862214d0bbf9a296669a583a2ef1aa7bfe05ab48
Kevin Boettler <kboettler@users.noreply.github.com>
2021-07-10 18:19:01 -0400

    Add unit tests for expected and edge cases (#1)
    Co-authored-by: Kevin Boettler <kpboettler28@gmail.com>

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace appApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2 ||
+                !uint.TryParse(args[1], out uint length))
+            {
+                Console.WriteLine("Expected two arguments for input string and substring length");
+                return;
+            }
+
+            var input = args[0];
+            foreach (var (substring, count) in Patterns.CountPatterns(input, length))
+            {
+                Console.WriteLine($"{substring}: {count}");
+            }
+        }
+    }
+}
