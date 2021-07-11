e642e9a19f58159db86b0e8f83b5fe62db296caf
Kevin Boettler <kpboettler28@gmail.com>
2021-07-11 13:21:19 -0400

    additional tests to ensure special characters are handled gracefully

--- test/Substrings.Tests/SubstringsTests.cs
+++ test/Substrings.Tests/SubstringsTests.cs
@@ -20,6 +20,7 @@ namespace Substrings.Tests
         [InlineData("a", 1, new string[] {"a"})]
         [InlineData("ab", 2, new string[] {"ab"})]
         [InlineData("ab", 1, new string[] {"a", "b"})]
+        [InlineData("aaa", 2, new string[] {"aa", "aa"})]
         public void Substrings_GivenInput_ExpectSubstrings(string input, uint length, IEnumerable<string> expectedSubstrings)
         {
             var result = Patterns.Substrings(input, length);
@@ -29,6 +30,15 @@ namespace Substrings.Tests
             {
                 Assert.Contains(expected, result);
             }
+        }
+
+        [Fact]
+        public void Substrings_SpecialCharacters_ExpectSubstrings()
+        {
+            var input = System.Environment.NewLine + System.Environment.NewLine;
+            var result = Patterns.Substrings(input, 1);
+
+            Assert.Equal(result.Count(), 2);
         }
 
         [Theory]
@@ -51,6 +61,14 @@ namespace Substrings.Tests
         {
             IEnumerable<(string pattern, uint count)> result = Patterns.CountPatterns(input, length);
             Assert.Equal(result.Single(r => r.pattern == checkPattern), (checkPattern, outputCount));
+        }
+
+        [Fact]
+        public void CountPatterns_SpecialCharacters_ExpectedOutput()
+        {
+            var input = System.Environment.NewLine + System.Environment.NewLine;
+            var result = Patterns.CountPatterns(input, 1);
+            Assert.Equal(result.Single().Item2, (uint)2);
         }
     }
 }
